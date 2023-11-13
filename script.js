bc84e3e91f3fd8a285ec5f886c4bfc6782a02c8f
Honorio-R <141484288+Honorio-R@users.noreply.github.com>
2023-11-13 14:42:59 +0800

    Add header and footer div without content Add the hover condition in Js and Css

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 256; i++) {
     // Create a new div element
     var newDiv = document.createElement('div');
-
+    
     // Add a class to the div
     newDiv.classList.add('grid-item');
 
     // Add second class to the div
     newDiv.classList.add('grid-' + i);
 
-    // Append the div to the body
+    // Append the div to the container
     container.appendChild(newDiv);
+
+    // Attach event listeners for hover effect inside the loop
+    newDiv.addEventListener('mouseover', function() {
+        this.classList.add('hovered');
+    });
+
+
+    // Removes the hover which reverts to the original style
+    // newDiv.addEventListener('mouseout', function() {
+    //     newDiv.classList.remove('hovered');
+    // });
 }
-            
