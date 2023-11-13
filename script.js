33642943070067be861807899a30c67205e60c39
Honorio-R <141484288+Honorio-R@users.noreply.github.com>
2023-11-13 23:57:50 +0800

    New: css -change drawed height and width in
    js
    -add and changePad function
    -put the for loop  in the new function
    -fix issue with pad not changing with function and dPad.innerHTML to empty

--- script.js
+++ script.js
@@ -11,44 +11,48 @@ let mouseHold = false; // Variable to check if mouse is being hold or pressed
 sliderVal.addEventListener('input', function(){
     outputVal.textContent = this.value + " x " + this.value;
     gridVal = this.value*this.value;
+    changePad();
 });
 
+function changePad(){
+    dPad.innerHTML = '';
+    // Creates 16x16 divs using for loop
+    for (let i = 0; i < gridVal; i++) {
+        // Create a new div element
+        const newDiv = document.createElement('div');
+        
+        // Add a class to the div
+        newDiv.classList.add('grid-item');
 
-// Creates 16x16 divs using for loop
-for (let i = 0; i < gridVal; i++) {
-    // Create a new div element
-    const newDiv = document.createElement('div');
-    
-    // Add a class to the div
-    newDiv.classList.add('grid-item');
+        // Add second class to the div
+        newDiv.classList.add('grid-' + i);
 
-    // Add second class to the div
-    newDiv.classList.add('grid-' + i);
+        // Append the div to the dPad
+        dPad.appendChild(newDiv);
 
-    // Append the div to the dPad
-    dPad.appendChild(newDiv);
-
-    // If mouse is pressed and hold mouseHold to true
-    newDiv.addEventListener('mousedown', function(event) {
-        event.preventDefault(); // Prevent default dragging issue
-        mouseHold = true;
-        this.classList.add('drawed');
-    });
-
-    // If mouse button is released mouseHold to false
-    newDiv.addEventListener('mouseup', function() {
-        mouseHold = false;
-    });
-
-    // If mouse is pressed within a div turns mouseHold to true
-    newDiv.addEventListener('mouseenter', function() {
-        if (mouseHold) {
+        // If mouse is pressed and hold mouseHold to true
+        newDiv.addEventListener('mousedown', function(event) {
+            event.preventDefault(); // Prevent default dragging issue
+            mouseHold = true;
             this.classList.add('drawed');
-        }
-    });
+        });
 
-    // Removes the hover which reverts to the original style
-    // newDiv.addEventListener('mouseout', function() {
-    //     newDiv.classList.remove('drawed');
-    // });
+        // If mouse button is released mouseHold to false
+        newDiv.addEventListener('mouseup', function() {
+            mouseHold = false;
+        });
+
+        // If mouse is pressed within a div turns mouseHold to true
+        newDiv.addEventListener('mouseenter', function() {
+            if (mouseHold) {
+                this.classList.add('drawed');
+            }
+        });
+
+        // Removes the hover which reverts to the original style
+        // newDiv.addEventListener('mouseout', function() {
+        //     newDiv.classList.remove('drawed');
+        // });
+    }
 }
+changePad();
